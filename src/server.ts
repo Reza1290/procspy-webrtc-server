7e5825cb33c6d64d783776640279b41671c9fbcf
Reza 1290 <reza.muktasib@gmail.com>
2025-05-04 06:11:55 +0700

    fix: authSocketio

--- src/server.ts
+++ src/server.ts
@@ -5,12 +5,14 @@ import { Server as SocketIOServer } from 'socket.io'
 import path from 'path'
 import indexRoutes from './routes/index-routes'
 import { handleSocketConnection } from './sockets/mediasoup-handler'
+import { env } from 'process'
 
-const PORT = 443 
+const PORT = 443
 
 const sslOptions = {
-  key: fs.readFileSync('/app/cert/key.pem'),
-  cert: fs.readFileSync('/app/cert/cert.pem'),
+  //TODO: FIX DIRECTORY
+  key: fs.readFileSync('./cert/key.pem'),
+  cert: fs.readFileSync('./cert/cert.pem'),
   rejectUnauthorized: false
 }
 
@@ -31,4 +33,39 @@ const io = new SocketIOServer(server, {
   },
 })
 
+// io.of('/mediasoup').use(async (socket, next) => {
+//   const token = socket.handshake.auth?.token;
+
+//   if (!token) {
+//     return next(new Error('Authentication token missing'));
+//   }
+
+//   const isValid = await verifyToken(token);
+
+//   if (!isValid) {
+//     return next(new Error('Invalid token'));
+//   }
+
+//   next();
+// });
 io.of('/mediasoup').on('connection', handleSocketConnection)
+
+
+const verifyToken = async (token: string): Promise<boolean> => {
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
+  try {
+    const response = await fetch(`${process.env.REST_ENDPOINT || env.REST_ENDPOINT || "http://192.168.2.5:5050"}/api/signin/${token}`)
+
+    const data = await response.json()
+    if (response.ok) {
+      if (data?.user) {
+        return true
+      }
+    }
+    return false
+
+  } catch (e) {
+    return false
+  }
+}
