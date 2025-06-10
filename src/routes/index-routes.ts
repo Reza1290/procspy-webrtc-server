db2a8124f4cae9f60d9fc2e5c801dd8c9ce8a4b8
Reza 1290 <reza.muktasib@gmail.com>
2025-06-10 21:03:00 +0700

    feat: route signin

--- src/routes/index-routes.ts
+++ src/routes/index-routes.ts
@@ -5,4 +5,28 @@ export default (router: Router): void => {
         res.send(`Hi This Is WebRTC Server :D Use Dashboard to join!`)
         return next()
     })
+
+    router.get('/api/signin/:token', async (req, res, next) => {
+        const authData = await auth(req.params.token)
+        res.send(authData)
+    })
+}
+
+const auth = async (token: string): Promise<any> => {
+    process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
+    try {
+        const response = await fetch(`${process.env.ENDPOINT || "http://192.168.2.5:5050"}/api/signin/${token}`)
+
+        const data = await response.json()
+        if (response.ok) {
+            if (data?.user) {
+                return data
+            }
+        }
+        return data
+
+    } catch (e) {
+    }
+    return { error: "" }
 }
