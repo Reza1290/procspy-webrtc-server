d8a664f7e45df8bfb1d5a5b4924341705560a7af
Reza 1290 <reza.muktasib@gmail.com>
2025-06-12 23:05:12 +0700

    log: warn

--- src/mediasoup/worker.ts
+++ src/mediasoup/worker.ts
@@ -13,6 +13,8 @@ async function createWorker(): Promise<Worker> {
   const worker = await (await import('mediasoup')).createWorker({
     rtcMinPort: Number(process.env.MIN_PORT) || 50000,
     rtcMaxPort: Number(process.env.MAX_PORT) || 60000,
+    logLevel: 'warn',
+    logTags: []
   })
 
   console.log(' Mediasoup worker created [PID]', worker.pid)
